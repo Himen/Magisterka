ef63da0a535084bb966fec2317b92bfc172ff1c1
Pawel Chmielewski <Pawel_Chmielewski@epam.com>
2014-08-11 15:21:56 +0200

    Add EF Entities Configurations

---
+++ HR.Web_UI/HR.Core/Enums/ProfesionType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Core.Enums
+{
+    [Flags]
+    public enum ProfesionType: byte
+    {
+        Programista_ASP_NET_MVC=1,
+        CEO= 2,
+        Human_Resource,
+        Programista_Java,
+        Programista_Cpp
+    }
+}
