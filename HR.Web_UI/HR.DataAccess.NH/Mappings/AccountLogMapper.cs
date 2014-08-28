60dac44b0593e9159e302e4f764dd52e5bf668ba
Pawe� Chmielewski <pawel.m.chmielewski@gmail.com>
2014-08-28 22:08:38 +0200

    Nhibernate accounts mappers

--- HR.Web_UI/HR.DataAccess.NH/Mappings/AccountLogMapper.cs
+++ HR.Web_UI/HR.DataAccess.NH/Mappings/AccountLogMapper.cs
@@ -5,6 +5,22 @@ namespace HR.DataAccess.NH.Mappings
 {
     public class AccountLogMapper : ClassMap<AccountLog>
     {
+        public AccountLogMapper()
+        {
+            Table("AccountLogs");
+            Schema("LOG");
+            Id(c => c.Id).GeneratedBy.Identity().CustomSqlType("bigint");
+            Map(c => c.DataState).CustomSqlType("tinyint");
+            Map(c => c.CreateDate).CustomSqlType("date");
+            Map(c => c.EditDate).CustomSqlType("date").Nullable();
+            Map(c => c.Action).CustomSqlType("varchar").Length(100);
+            Map(c => c.ActionType).CustomType<int>().CustomSqlType("smallint");
+            Map(c => c.ActionDescription).CustomSqlType("varchar").Length(100).Nullable();
+            Map(c => c.StartDate).CustomSqlType("date");
+            Map(c => c.EndDate).CustomSqlType("date");
 
+            //nhforge.org/blogs/nhibernate/archive/2008/09/06/a-fluent-interface-to-nhibernate-part-3-mapping-relations.aspx
+            //References(c=>c.Account).
+        }
     }
 }
--- HR.Web_UI/HR.DataAccess.NH/Mappings/AccountMapper.cs
+++ HR.Web_UI/HR.DataAccess.NH/Mappings/AccountMapper.cs
@@ -9,11 +9,18 @@ namespace HR.DataAccess.NH.Mappings
         {
             Table("Accounts");
             Id(c => c.Id).GeneratedBy.Identity().CustomSqlType("bigint");
-            Map(c => c.Password);
-            Map(c=>c.Photo);
-            Map(c=>c.UserName);
-            Map(c => c.AccountType).CustomType<int>().CustomSqlType(""); ;
-            Map(c=>c.DataState);
+            Map(c => c.DataState).CustomSqlType("tinyint");
+            Map(c => c.CreateDate).CustomSqlType("date");
+            Map(c => c.EditDate).CustomSqlType("date").Nullable();
+            Map(c => c.UserName).CustomSqlType("varchar").Length(80).Unique();
+            Map(c => c.Password).CustomSqlType("varchar").Length(20);
+            Map(c => c.AccountType).CustomType<int>().CustomSqlType("smallint");
+            Map(c=>c.Photo).CustomSqlType("image").Nullable();
+
+            HasMany<AccountLog>(c => c.AccountLogs).Cascade.AllDeleteOrphan();
+
+            //powinno wystarczyc
+            References(c=>c.Person)
         }
     }
 }
