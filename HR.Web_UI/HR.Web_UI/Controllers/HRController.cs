480d74f398f361da970d6806d3d3261ed900e98d
Pawel Chmielewski <pawel.m.chmielewski@gmail.com>
2014-08-28 21:36:13 +0200

    Minor changes

--- HR.Web_UI/HR.Web_UI/Controllers/HRController.cs
+++ HR.Web_UI/HR.Web_UI/Controllers/HRController.cs
@@ -673,7 +673,7 @@ namespace HR.Web_UI.Controllers
                 .SetSeries(new Series
                 {
                     Data = new Data(new object[] { 29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4 })
-                }).SetLegend(new Legend().);
+                });
 
             return View(chart);
         }
